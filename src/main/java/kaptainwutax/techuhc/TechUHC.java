3ba13be41c2d007e550762570ce45220e6e329dd
Unknown <kaptainwutax@gmail.com>
2019-08-16 21:04:47 -0400

    Combat Update 2

---
+++ src/main/java/kaptainwutax/techuhc/TechUHC.java
@@ -0,0 +1,11 @@
+package kaptainwutax.techuhc;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TechUHC implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+
+}
